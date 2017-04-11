7f966c19c2919f2bf5bdf80ef93f197772b1a259
housemeow <housemeow@gmail.com>
2017-04-11 17:05:41 +0800

    disable log

--- onelook/content.js
+++ onelook/content.js
@@ -1,6 +1,5 @@
 document.addEventListener("mouseup", function(){
 	var selection = window.getSelection().toString();
-	console.log("selection:" + selection);
 	chrome.runtime.sendMessage({
 		selection: selection
 	});
